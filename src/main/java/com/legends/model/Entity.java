d1aa701333d495c1fd9e126e8a103971591a3f02
Adithya <adithya608@gmail.com>
2025-11-20 21:29:33 -0500

    Add Entity class as an abstract base for game entities with properties for name, level, and health management

---
+++ src/main/java/com/legends/model/Entity.java
@@ -0,0 +1,40 @@
+package com.legends.model;
+
+public abstract class Entity {
+    protected String name;
+    protected int level;
+    protected int hp;
+
+    public Entity(String name, int level) {
+        this.name = name;
+        this.level = level;
+        this.hp = level * 100; // Base HP calculation, can be overridden
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public boolean isAlive() {
+        return hp > 0;
+    }
+
+    public void takeDamage(int damage) {
+        this.hp -= damage;
+        if (this.hp < 0) {
+            this.hp = 0;
+        }
+    }
+}
