72b5a02e3911af1fa8b0630be7c8087d6add6214
Adithya <adithya608@gmail.com>
2025-12-15 13:41:10 -0500

    Implement Serializable interface for MonsterAI

--- src/main/java/com/legends/ai/MonsterAI.java
+++ src/main/java/com/legends/ai/MonsterAI.java
@@ -3,11 +3,12 @@ package com.legends.ai;
 import com.legends.model.Monster;
 import com.legends.board.ValorBoard;
 import com.legends.io.Output;
+import java.io.Serializable;
 
 /**
  * Strategy pattern will be used through this interface.
  */
-public interface MonsterAI {
+public interface MonsterAI extends Serializable {
 
     /**
      * Perform exactly one monster action this turn.
