7de901cfc7ef416e9645c78c2fc8bca67b97b687
Adithya <adithya608@gmail.com>
2025-12-16 00:52:55 -0500

    Fix actionTaken assignment for potion use and equipment change in battle actions

--- src/main/java/com/legends/battle/Battle.java
+++ src/main/java/com/legends/battle/Battle.java
@@ -166,10 +166,10 @@ public class Battle {
                         turnTaken = performCastSpell(hero);
                         break;
                     case "3":
-                        performUsePotion(hero);
+                        turnTaken = performUsePotion(hero);
                         break;
                     case "4":
-                        performChangeEquipment(hero);
+                        turnTaken = performChangeEquipment(hero);
                         break;
                     default:
                         output.println("Invalid action. Please try again.");
