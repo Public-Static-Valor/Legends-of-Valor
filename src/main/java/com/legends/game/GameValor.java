d8a9af67faa3bc1206a8188d7bb47f902497a173
Adithya <adithya608@gmail.com>
2025-12-16 00:50:12 -0500

    Fix actionTaken assignment for potion use and equipment change in game actions

--- src/main/java/com/legends/game/GameValor.java
+++ src/main/java/com/legends/game/GameValor.java
@@ -250,10 +250,10 @@ public class GameValor extends RPGGame {
                         actionTaken = handleHeroCastSpell(hero);
                         break;
                     case "3":
-                        handlePotionUse(hero);
+                        actionTaken = handlePotionUse(hero);
                         break;
                     case "4":
-                        handleHeroChangeEquipment(hero);
+                        actionTaken = handleHeroChangeEquipment(hero);
                         break;
                     case "5":
                         actionTaken = handleDestroyObstacle(hero);
